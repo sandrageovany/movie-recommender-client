9805361d8dca09526c0fa42987a3b6854aa3dc08
sandrageovany <sandra.geovany@gmail.com>
2022-12-08 17:22:47 +0200

    Healthcheck compenent displays ok

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Healthcheck from "./Components/Healthcheck";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h3>Movie recommender app</h3>
-        <p>under construction...</p>
-      </header>
-      <Switch>
-          {/* <Route exact path={'/trips', '/'} component={AllTrips} /> */}
-          <Route exact path='/healthcheck' component={Healthcheck} />
-          </Switch>
-    </div>
+    <Routes>
+      <Route exact path="/healthcheck" element={<Healthcheck/>} />
+    </Routes>
   );
 }
 
---
+++ src/Components/Healthcheck.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+class Healthcheck extends React.Component {
+    render() {
+      return <h2>Ok</h2>;
+    }
+  }
+export default Healthcheck;
